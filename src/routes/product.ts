968ee6dde5749d5b51782d057820aba09ff7f2ec
lanle <lanle101017@gmail.com>
2024-09-27 03:14:45 +0700

    upload code

--- src/routes/product.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getProductByID } from '../controllers/productDetail';
-
-const router = express.Router();
-
-router.get('/:id', getProductByID);
-
-export default router;
